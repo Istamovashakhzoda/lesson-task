4450eeec593b61fb9204806a999283b9867fffe1
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-08 15:27:58 +0300

    Add files via upload

---
+++ lesson-3/strict.js
@@ -0,0 +1,43 @@
+"use script"
+let a =Number(prompt("delfinlar"));
+//let b = Number(prompt("koalalar"))
+let d=(96);
+//alert(d);
+let f=(108);
+//alert(f);
+let e=(98);
+//alert(e);
+let i=(97);
+//alert(i);
+let k=(112);
+//alert(k);
+let j=(101);
+//alert(j);
+let main1=(((d+f)+(e+i)+(k+j)) /3);
+alert(main1);
+//let a =Number(prompt("delfinlar"));
+let b = Number(prompt("koalalar"));
+let m=(88);
+//alert(d);
+let n=(91);
+//alert(f);
+let l=(110);
+//alert (e);
+let t=(109);
+//alert(i);
+let s=(95);
+//alert(k);
+let p=(100);
+//alert(j);
+let main2=(((m+n)+(l+t)+(s+p)) /3);
+alert(main2);
+
+
+if(main1>main2){
+alert(main1);
+}else if(main1<main2){
+alert(main2);
+}else if(delfinlar);
+
+
+
